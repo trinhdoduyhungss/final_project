4358afae64d3c13769ca7a9e240bb242cf2f3127
trinhdoduyhungss@gmail.com <trinhdoduyhungss@gmail.com>
2018-08-18 09:13:17 +0700

    test

--- src/screens/login/components/LoginScreen.js
+++ src/screens/login/components/LoginScreen.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import { 
-  StyleSheet, 
   View, 
   Button,
   Text,
   TextInput,
   Alert
 } from 'react-native';
-//import {databaseacc} from '@login/components/LoginScreen';
 import * as firebase from 'firebase';
 import * as c from "@thread/component/constants"
-// var firebase = require('firebase')
 const config = {
     apiKey: c.FIREBASE_API_KEY,
     authDomain: c.FIREBASE_AUTH_DOMAIN,
@@ -28,11 +25,9 @@ export default class LoginScreen extends Component {
     this.Signin = this.Signin.bind(this);
   }
   Signin(user, pass){
-    // var verifyformat = user.findIndex("@gmail.com"||"@gmail.com.vn") > -1
-    //console.log(databaseacc)
     console.log(user +' '+pass)
     verifyuser=firebase.database().ref(user)
-    verifyuser.on('value', snapshot =>{
+    verifyuser.on('value', snapshot => {
         console.log(snapshot.val())
         if(pass == snapshot.val() && pass != '' && snapshot.val() != ''){
             this.props.navigation.navigate('HomeScreen')
@@ -46,21 +41,10 @@ export default class LoginScreen extends Component {
             { cancelable: false })
         }
     })
-    // if(verifyformat){
-    // }else{
-    //     Alert.alert(
-    //         'Dang nhap khong thanh cong',
-    //         'Xem lai tai khoan hoac mat khau',
-    //         [
-    //           {text: 'OK', onPress: () => console.log('OK Pressed')},
-    //         ],
-    //         { cancelable: false })
-    // }
   }
   render() {
     return (
       <View>
-        {/* {renderProduct(this.props.list, this.props.deleteListProduct)} */}
         <TextInput
         style={{height: 40, borderColor: 'red', borderWidth: 2}}
         onChangeText={(user) => this.setState({user})}
@@ -79,19 +63,3 @@ export default class LoginScreen extends Component {
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'flex-start',
-//     alignSelf: 'center',
-//     marginTop: 30,
-//     // flexDirection: 'row'
-//   },
-//   textList: {
-//     fontSize: 30,
-//     marginBottom: 20,
-//     fontWeight: 'bold'
-//   }
-// });
