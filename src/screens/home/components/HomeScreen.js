e070dd1e9c7250f86e79c756b9d9b8995e5d33c1
trinhdoduyhungss@gmail.com <trinhdoduyhungss@gmail.com>
2018-08-19 13:06:53 +0700

    test

--- src/screens/home/components/HomeScreen.js
+++ src/screens/home/components/HomeScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import {key} from '@login/components/LoginScreen'
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>{key}</Text> */}
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
